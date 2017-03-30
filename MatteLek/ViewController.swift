3d350a42c5015f09c0c80708cba8121cc453f12b
timmy <timmykonneus@gmail.com>
2017-03-30 17:02:05 +0200

    Initial Commit

---
+++ MatteLek/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MatteLek
+//
+//  Created by TIMMY KONNEUS on 30/03/17.
+//  Copyright © 2017 TIMMY KONNEUS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
